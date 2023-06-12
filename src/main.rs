fa2e2a625581352afc71186cfbcd21fd35aa660c
Khoa Hoang <koadahoakodah@gmail.com>
2023-06-11 23:31:05 -0400

    move some schedules around

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,14 @@ mod macros;
 mod systems;
 use systems::*;
 
-
-fn test_system() {
-    println!("testing");
+fn setup_graphics() {
+    println!("setup graphics");
 }
 
 fn main() {
     EcsBuilder::new()
         .add_resource(Time { current: 0.0, delta: 0.0 })
-        .add_system(test_system, Startup)
+        .add_system(setup_graphics, StartupSingleThreaded)
         .set_runner(runner)
         .run();
 }
@@ -35,20 +34,27 @@ fn runner(mut ecs: Ecs) {
     let (mut window, event_loop) = window::Window::new();
     let renderer = Arc::new(Mutex::new(None));
 
-    ecs.run_schedule(StartupSingleThreaded);
-    ecs.run_schedule(Startup);
-
     event_loop.run(move |event, window_target, control_flow| {
         control_flow.set_wait();
         match event {
             Event::Resumed => {
-                // Make the window's context current and initialize some other things in Window
-                window.on_resumed(window_target);
-
-                // Initialize the Renderer after the window's context is current
                 let mut guard = renderer.lock().unwrap();
-                guard.get_or_insert_with(|| Renderer::new(&window));
-                ecs.insert_non_send_resource(renderer.clone());
+                match guard.as_ref() {
+                    // Return if Renderer has already been initialized
+                    Some(_) => (),
+                    None => {
+                        // Make the window's context current and initialize some other things in Window
+                        window.on_resumed(window_target);
+
+                        // Initialize the Renderer after the window's context is current
+                        guard.get_or_insert_with(|| Renderer::new(&window));
+                        ecs.insert_non_send_resource(renderer.clone());
+
+                        // Run startup schedules
+                        ecs.run_schedule(StartupSingleThreaded);
+                        ecs.run_schedule(Startup);
+                    },
+                };
             },
             Event::Suspended => window.on_suspended(),
             Event::WindowEvent { event, .. } => match event {
